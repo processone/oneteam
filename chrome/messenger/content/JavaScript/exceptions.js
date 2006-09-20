b89e68685df35a9eaa5a71476931e55a4833e603
Paweł Chmielowski <pchmielowski@proces-one.net>
2006-09-20 11:55:50 +0000

    More utility functions.

---
+++ chrome/messenger/content/JavaScript/exceptions.js
@@ -0,0 +1,178 @@
+function dumpStack(stackFrame, indent)
+{
+    var stacktrace = "";
+
+    stackFrame = stackFrame || Components.stack.caller;
+    indent = indent || "";
+
+    while (stackFrame && stackFrame.caller) {
+        stacktrace += indent+stackFrame.name+"(<unknown>) at "+
+            (stackFrame.filename||"<unknown>")+":"+
+            (stackFrame.lineNumber||"<unknown>")+"\n";
+        stackFrame = stackFrame.caller;
+    }
+    return stacktrace;
+}
+
+/**
+ *   Returns human readable text representation of exception.
+ *
+ *  @tparam Exception exc Exception to process.
+ *  @tparam String indent Text which will be appended on begin of each
+ *    line. Can be *null*
+ *
+ *  @treturn String String representation of exception passed as \em
+ *    exc.
+ */
+function exceptionToString(exc, indent)
+{
+    var frames, stacktrace, pos, i, msg, tmp;
+
+    indent = indent || "";
+
+    if (exc) {
+        if (exc instanceof Components.interfaces.nsIException) {
+            msg = indent+"Exception '"+exc.message+"' - "+
+            exc.result+"("+exc.name+") thrown at " +
+            exc.filename+":"+exc.lineNumber+"\n";
+
+            if (exc.inner)
+                msg += indent+"Caused by:\n" +
+                    arguments.callee(exc.inner, indent+"  ");
+
+            return msg+indent+"Stacktrace:\n"+
+                dumpStack(exc.location, indent+"   ");
+        } else if (exc instanceof Error || exc.stack) {
+            tmp = exc.stack.split("\n");
+            frames = [];
+
+            for (i = 0; i < tmp. length; i++) {
+                var m = tmp[i].lastIndexOf("@");
+                frames[i] = [tmp[i].slice(0, m), tmp[i].slice(m+1)];
+            }
+
+            stacktrace = "";
+            pos = exc.fileName+":"+exc.lineNumber;
+
+            for (i = 0; i < frames.length && frames[i][1] != pos; i++)
+                ;
+
+            for (; i < frames.length-2; i++)
+                stacktrace += indent+"  "+frames[i][0]+
+                    " at "+frames[i][1]+"\n";
+
+            msg = indent+"Exception '"+exc.message+"' thrown at " +
+            exc.fileName+":"+exc.lineNumber +"\n";
+
+            if (exc.reason)
+                msg += indent+"Caused by:\n" +
+                    arguments.callee(exc.reason, indent+"  ");
+
+            return msg+indent+"Stacktrace:\n"+stacktrace;
+        } else if (exc.code)
+            return indent+"Exception code: "+exc.code+"\n"+
+                (exc.reason ? indent+"Caused by:\n" +
+                arguments.callee(exc.reason,indent+"  ")+"\n":"");
+        else
+            return indent+uneval(exc);
+    } else
+    	return indent+"Null exception\n";
+}
+
+function logExceptionInConsole(exc)
+{
+    var cs = Components.classes["@mozilla.org/consoleservice;1"].
+        getService(Components.interfaces.nsIConsoleService);
+    var se = Components.classes["@mozilla.org/scripterror;1"].
+        createInstance(Components.interfaces.nsIScriptError);
+    var msg = exceptionToString(exc);
+    var file = exc && (exc.fileName || exc.filename);
+    var line = exc && exc.lineNumber;
+
+    se.init(msg, file, null, line, 0, 0, "component");
+    cs.logMessage(se);
+}
+
+Error.prototype.toString = function() {
+    return exceptionToString(this, "");
+}
+
+Components.interfaces.nsIException.toString = Error.prototype.toString;
+
+/**
+ * Base class for all JabberZilla exception classes.
+ *
+ * \anchor GenericError_inheritance
+ * This class needs special kind of inheritance, just use this snippet:
+ * \code
+ *   function MyError(message, reason) {
+ *     return GenericError.call(this, message||"MyError", reason)
+ *   }
+ * \endcode
+ * @ctor
+ * Create new generic exception object. This Exception is based on
+ *   built-in Error object (thanks to this, we have access to stack
+ *   traces).
+ *
+ * @tparam String message Message associated with exception.
+ * @tparam Error reason Exception which causes this exception. Can be *null*
+ */
+function GenericError(message, reason) {
+    var exc;
+    var stack = Components.stack.caller;
+    var fun = arguments.callee;
+
+    do {
+
+        while (stack && stack.filename == null)
+            stack = stack.caller;
+
+        if (!stack) {
+            stack = Components.stack.caller;
+            break;
+        }
+
+        if (fun.prototype === this.__proto__)
+            break;
+        fun = fun.caller;
+        stack = stack.caller;
+    } while (true);
+
+    exc = new Error(message,
+		    stack.filename,
+		    stack.lineNumber);
+    exc.__proto__ = this.__proto__;
+
+    if (reason)
+    	exc.reason = reason;
+    return exc;
+}
+
+GenericError.prototype =
+{
+    __proto__: Error.prototype
+};
+
+/**
+ * Exception which should be thrown when invalid argument is passed.
+ * \see \ref GenericError_inheritance "GenericError" for inheritance
+ * instructions.
+ *
+ * @ctor
+ *
+ * Create new InvalidArgsError exception;
+ *
+ * @tparam String message Message associated with exception.
+ * @tparam Error reason Exception which causes this exception. Can be *null*
+ */
+function InvalidArgsError(message, reason) {
+    return GenericError.call(this, message||"InvalidArgsError", reason);
+}
+
+InvalidArgsError.prototype =
+{
+    __proto__: GenericError.prototype,
+}
+
+//function registerExceptionCatcher() {
+//}
---
+++ chrome/messenger/content/JavaScript/file.js
@@ -0,0 +1,717 @@
+/**
+ * Converts strings between unicode and other encoding schemes.
+ *
+ * @ctor
+ *
+ * Create new CharsetConverter object.
+ *
+ * @tparam String charset String with name of external encoding.
+ */
+function CharsetConverter(charset)
+{
+    this.charset = charset;
+    try {
+        this.__converter = Components.classes[
+            "@mozilla.org/intl/scriptableunicodeconverter"].
+        getService(Components.interfaces.
+            nsIScriptableUnicodeConverter);
+    } catch (ex) {
+        throw new CharsetConvertError(
+            "CharsetConverter: unable to instantiate interface", ex);
+    }
+}
+
+CharsetConverter.prototype =
+{
+    /**
+     * Name of external encoding.
+     *
+     * @type String
+     * @public
+     */
+    charset: null,
+
+    /**
+     * Convert string from unicode to external encoding scheme.
+     *
+     * @tparam String data String to encode.
+     *
+     * @treturn String Encoded string.
+     *
+     * @public
+     */
+    encode: function(data)
+    {
+        if (this.charset) {
+            try {
+                this.__converter.charset = this.charset;
+                return this.__converter.ConvertFromUnicode(data);
+            } catch (ex) {
+                throw new CharsetConvertError(
+                    "CharsetConverter.encode: conversion error", ex);
+            }
+        }
+        return data;
+    },
+
+    /**
+     * Convert string from external encoding scheme to unicode.
+     *
+     * @tparam String data String to convert.
+     *
+     * @treturn String Converted string.
+     *
+     * @public
+     */
+    decode: function(data)
+    {
+        if (this.charset) {
+            try {
+                this.__converter.charset = this.charset;
+                return this.__converter.ConvertToUnicode(data);
+            } catch (ex) {
+                throw new CharsetConvertError(
+                    "CharsetConverter.decode: conversion error", ex);
+            }
+        }
+        return data;
+    },
+}
+
+/**
+ * This is local variable
+ * @static
+ */
+var magic = [
+    [/^\xEF\xBB\xBF/,     "UTF-8",    3],
+    [/^\x3C\x3F\x78\x6D/, null,       0],
+    [/^\x00\x00\xFE\xFF/, "UTF-32BE", 4],
+    [/^\x00\x00\x00\x3C/, "UTF-32BE", 0],
+    [/^\xFE\xFF\x00\x00/, "UTF-32LE", 4],
+    [/^\x3C\x00\x00\x00/, "UTF-32LE", 0],
+    [/^\xFE\xFF/,         "UTF-16BE", 2],
+    [/^\x00\x3C\x00\x3F/, "UTF-16BE", 0],
+    [/^\xFF\xFE/,         "UTF-16LE", 2],
+    [/^\x3C\x00\x3F\x00/, "UTF-16LE", 0],
+    [/./,                 "UTF-8",    0]
+];
+
+/**
+ * Attempt to determine charset from xml prolog.
+ *
+ * @tparam String data String which contains xml prolog.
+ * @tparam bool skipOctets Object in which xml prolog length will
+ *   be stores as property \em value. <em>(optionam)</em>
+ *
+ * @treturn String String with name of encoding, or \c null if data is
+ *   not sufficient to determine encoding.
+ */
+function detectXMLCharset(data, skipOctets)
+{
+    var i, converter, charset, res;
+
+    for (i = 0; i < magic.length; i++)
+        if (!data.search(magic[i][0]))
+            if (magic[i][1]) {
+                if (typeof(skipOctets) == "object")
+                    skipOctets.value = magic[i][2];
+                return magic[i][1];
+            } else
+                break;
+
+    res = data.match(/^<\?xml\s+[^>]*encoding\s*=\s*(?:\"([^>\"]*)\"|\'([^>\']*)\')/);
+
+    if ((idx = data.indexOf(">")) >= 0 || res) {
+        if (typeof(skipOctets) == "object")
+            skipOctets.value = magic[i][2];
+        return res&&(!~idx||idx > res.index) ? res[1]||res[2]||"UTF-8" : "UTF-8";
+    }
+    return null;
+}
+
+/**
+ * Converts xml data. Attempt to determine charset from xml prolog.
+ *
+ * @tparam String data String to convert.
+ *
+ * @treturn String Data from \c data converted to unicode.
+ */
+function convertCharsetOfXMLData(data)
+{
+    var skipOctets = {};
+    var charset = detectXMLCharset(data, skipOctets);
+
+    if (!charset)
+        throw new CharsetConvertError(
+            "convertCharsetOfXMLData: unable to determine charset");
+
+    try {
+        converter = Components.classes[
+            "@mozilla.org/intl/scriptableunicodeconverter"].
+            getService(Components.interfaces.nsIScriptableUnicodeConverter);
+    } catch (ex) {
+        throw new CharsetConvertError(
+            "convertCharsetOfXMLData: unable to instantiate interface", ex);
+    }
+
+    converter.charset = charset;
+    if (skipOctets.value == 0)
+        return converter.ConvertToUnicode(data);
+    else
+        return converter.ConvertToUnicode(data.substring(skipOctets.value));
+}
+
+/**
+ * Thrown when errors occurs during charset conversion of string.
+ *
+ * @ctor
+ *
+ * Create new CharsetConvertError
+ *
+ * @tparam String desc Message associated with exception. \e (optional)
+ * @tparam Error reason Exception which causes this exception.
+ * \e (optional)
+ */
+function CharsetConvertError(desc, reason)
+{
+    return GenericError.call(this, message||"CharsetConvertError", reason);
+}
+
+CharsetConvertError.prototype =
+{
+    __proto__: GenericError.prototype,
+}
+
+
+/**
+ * Class created to read data from stream speficied by uri.
+ *
+ * @ctor
+ *
+ * Creates new Reader object.
+ *
+ * @tparam  String  uriString String with uri which specifies input
+ *   streams.
+ *
+ * @throws IOError Throws when \e uriString don't containt valid data.
+ */
+function Reader(uriString)
+{
+    var ios, tmp, i;
+
+    try {
+        ios = classes["@mozilla.org/network/io-service;1"].
+            getService(interfaces.nsIIOService);
+    } catch (ex) {
+        throw new IOError("Reader: unable to instantiate interface", ex);
+    }
+
+    if (uriString) {
+        if (uriString instanceof interfaces.nsIInputStream) {
+            try {
+                var bs = classes["@mozilla.org/binaryinputstream;1"].
+                    createInstance(interfaces.nsIBinaryInputStream);
+
+                bs.setInputStream(uriString);
+
+                this.__inputStream = classes["@mozilla.org/scriptableinputstream;1"].
+                    createInstance(interfaces.nsIScriptableInputStream);
+
+                this.__inputStream.init(bs);
+            } catch (ex) {
+        	    throw new IOError("Reader: Initialization error", ex);
+            }
+
+            return;
+        }
+        if (typeof(uriString) == 'object') {
+            tmp = uriString[0];
+            for (i = 1; i < uriString.length; i++) {
+                tmp += "/"+uriString[i];
+            }
+            uriString = tmp;
+        }
+        try {
+            this.uri = ios.newURI(uriString, null, null);
+        } catch (ex) {
+    	    throw new IOError("Reader: invalid uri", ex);
+        }
+    }
+}
+
+Reader.prototype =
+{
+    __inputStream: null,
+    __encoder: null,
+
+    /**
+     * Length of maximal available data segment. For file streams this
+     * property contains length of file. <em>(read-only)</em>
+     *
+     * @type int
+     * @public
+     */
+    get available ()
+    {
+        if (this.__inputStream)
+            return this.__inputStream.available();
+        return 0;
+    },
+
+    /**
+     * Open streams.
+     *
+     * @tparam  String  charset String with charset id of data in
+     *   stream. If specified, all readed data will be converted
+     *   from format described by this parameter, to internal unicode format.
+     *
+     * @throws IOError Thrown when any IO error occurs.
+     *
+     * @public
+     */
+    open: function(charset)
+    {
+        if (this.__inputStream)
+            return;
+
+        var ios = classes["@mozilla.org/network/io-service;1"].
+            getService(interfaces.nsIIOService);
+        this.setCharset(charset);
+
+        var channel = ios.newChannelFromURI(this.uri);
+        if (!channel)
+            throw new IOError("Reader.open: unable to create channel");
+
+        try {
+            var is = channel.open();
+
+            this.__binaryInputStream = classes["@mozilla.org/binaryinputstream;1"].
+                createInstance(interfaces.nsIBinaryInputStream);
+
+            this.__binaryInputStream.setInputStream(is);
+
+            this.__inputStream = classes["@mozilla.org/scriptableinputstream;1"].
+                createInstance(interfaces.nsIScriptableInputStream);
+
+            this.__inputStream.init(is);
+        } catch (ex) {
+            throw new IOError("Reader.open: unable to create stream", ex);
+        }
+    },
+
+    /**
+     * Set charset used to convert data from stream.
+     *
+     * @tparam  String  charset Name of charset used by converter.
+     *
+     * @public
+     */
+    setCharset: function(charset)
+    {
+        if (charset) {
+            if (this.__coder)
+                this.__coder.charset = charset;
+            else
+                this.__coder = new CharsetConverter(charset);
+        } else
+            this.__coder = null;
+    },
+
+    /**
+     * Reads data from stream.
+     *
+     * @tparam  int  length  Size of data to read. This value will be
+     *   trimed to length of current available data segment. Default
+     *   value for this parameter is value of #available property.
+     *   <em>(optional)</em>
+     *
+     * @treturn  String  Data readed from stream.
+     *
+     * @throws IOError Thrown when any IO error occurs.
+     * @throws CharsetConvertError Thrown when charset conversion fails.
+     *
+     * @public
+     */
+    read: function(length)
+    {
+        var result;
+
+        try {
+            var available = this.__inputStream.available();
+
+            if (!length || length > available)
+                length = available;
+
+            result = this.__binaryInputStream.readBytes(length);
+        } catch (ex) {
+            throw new IOError("Reader.read: read error", ex);
+        }
+
+        if (this.__coder)
+            return this.__coder.decode(result);
+        return result;
+    },
+
+    /**
+     * Close stream.
+     *
+     * @public
+     */
+    close: function()
+    {
+        if (this.__inputStream)
+            this.__inputStream.close();
+        this.__inputStream = null;
+    }
+}
+
+/**
+ * Thrown when errors occurs in any IO related object.
+ *
+ * @ctor
+ *
+ * Create new IOError exception.
+ *
+ * @tparam  String  message  Message associated with exception.
+ *   <em>(optional)</em>
+ * @tparam  Exception  reason  Exception which causes this exception.
+ *   <em>(optional)</em>
+ */
+function IOError(message, reason, ommit)
+{
+    return GenericError.call(this, message||"IOError", reason);
+}
+
+IOError.prototype =
+{
+    __proto__: GenericError.prototype
+}
+
+/**
+ * File access class.
+ *
+ * @ctor
+ *
+ * Create new File object.
+ *
+ * @tparam  String  path  File path. This can be also \c nsIURI object
+ * with "file" scheme.
+ *
+ * @throws  IOError Thrown on IO errors.
+ */
+function File(path)
+{
+    const classes = Components.classes;
+    const interfaces = Components.interfaces;
+
+    var uri, i;
+    var ios;
+
+    try {
+        ios = classes["@mozilla.org/network/io-service;1"].
+            getService(interfaces.nsIIOService);
+
+        this.file = classes["@mozilla.org/file/local;1"].
+            createInstance(interfaces.nsILocalFile);
+
+        if (path instanceof File) {
+            this.file.initWithPath(path.path);
+
+            for (i = 1; i < arguments.length; i++)
+                this.file.append(arguments[i]);
+
+            this.uri = ios.newFileURI(this.file);
+        } else if (path.search(/\w+:\/\//) == -1) {
+            this.file.initWithPath(path);
+
+            for (i = 1; i < arguments.length; i++)
+                this.file.append(arguments[i]);
+
+            this.uri = ios.newFileURI(this.file);
+        } else {
+            Reader.call(arguments);
+
+            this.file.initWithPath(this.uri.
+                QueryInterface(interfaces.nsIFileURL).file.path);
+        }
+    } catch (ex) {
+        throw new IOError("File: unable to create file object", ex);
+    }
+
+    this.path = this.file.path;
+}
+
+File.prototype =
+{
+    __proto__: Reader.prototype,
+
+    /**
+     * Flag used by open() method.
+     *
+     * @type int
+     * @public
+     */
+    MODE_RDONLY   : 0x01,
+    /**
+     * Flag used by open() method.
+     *
+     * @type int
+     * @public
+     */
+    MODE_WRONLY   : 0x02,
+    /**
+     * Flag used by open() method.
+     *
+     * @type int
+     * @public
+     */
+    MODE_RDWR     : 0x04,
+    /**
+     * Flag used by open() method.
+     *
+     * @type int
+     * @public
+     */
+    MODE_CREATE   : 0x08,
+    /**
+     * Flag used by open() method.
+     *
+     * @type int
+     * @public
+     */
+    MODE_APPEND   : 0x10,
+    /**
+     * Flag used by open() method.
+     *
+     * @type int
+     * @public
+     */
+    MODE_TRUNCATE : 0x20,
+    /**
+     * Flag used by open() method.
+     *
+     * @type int
+     * @public
+     */
+    MODE_SYNC     : 0x40,
+    /**
+     * Flag used by open() method.
+     *
+     * @type int
+     * @public
+     */
+    MODE_EXCL     : 0x80,
+
+    /**
+     * This file uri.
+     *
+     * @type nsIURI.
+     * @public
+     */
+    uri: null,
+
+    /**
+     * Implementation of \e nsILocalFile interface used to handle this
+     *   file object.
+     *
+     * @type nsILocalFile
+     * @public
+     */
+    file: null,
+
+    /**
+     * String with full path to our file.
+     *
+     * @type String
+     * @public
+     */
+    path: null,
+
+    /**
+     * \c true if file (or directory) with this path exists.
+     *
+     * @type bool
+     * @public
+     */
+    get exists ()
+    {
+        return this.file.exists();
+    },
+
+    /**
+     * Size of file.
+     *
+     * @type int
+     * @public
+     */
+    get size()
+    {
+        return this.file.fileSize;
+    },
+
+    /**
+     * Open file.
+     *
+     * @tparam  String  charset  Name of charset in which file is coded.
+     *     Reader.read() and write() methods performs conversion
+     *     in-the-fly. <em>(optional)</em>
+     * @tparam  int     mode     Open mode, bit mask of \e MODE_*
+     *   attributes.
+     * @tparam  int     perms    Unix like file permisions code. Used on
+     *   file creation. By default 0644 value is used.
+     *   <em>(optional)</em>.
+     *
+     * @throws  IOError  Thrown on any IO error.
+     *
+     * @public
+     */
+    open: function(charset, mode, perms)
+    {
+        const classes = Components.classes;
+        const interfaces = Components.interfaces;
+
+        this.setCharset(charset);
+
+        if (typeof perms == "undefined")
+            perms = 0644;
+
+        this.__inputStream = this.__outputStream = null;
+
+        if (mode & (this.MODE_WRONLY | this.MODE_RDWR))
+        {
+            try {
+                this.__outputStream =
+                    classes["@mozilla.org/network/file-output-stream;1"].
+                    createInstance(interfaces.nsIFileOutputStream);
+                this.__outputStream.init(this.file, mode, perms, false);
+            } catch (ex) {
+                this.__inputStream = this.__outputStream = null;
+
+                throw new IOError("File.open: unable to create output stream",
+                    ex);
+            }
+        }
+
+        if (mode & (this.MODE_RDONLY | this.MODE_RDWR))
+        {
+            try {
+                var is = classes["@mozilla.org/network/file-input-stream;1"].
+                    createInstance(interfaces.nsIFileInputStream);
+
+                this.__binaryInputStream = classes[
+                    "@mozilla.org/binaryinputstream;1"].
+                    createInstance(interfaces.nsIBinaryInputStream);
+
+                is.init(this.file, mode, perms, false);
+                this.__binaryInputStream.setInputStream(is);
+
+                this.__inputStream = classes["@mozilla.org/scriptableinputstream;1"].
+                    createInstance(interfaces.nsIScriptableInputStream);
+
+                this.__inputStream.init(is);
+            } catch (ex) {
+                if (this.__outputStream)
+                    this.__outputStream.close();
+                this.__inputStream = this.__outputStream = null;
+
+                throw new IOError("File.open: unable to create input stream",
+                    ex);
+            }
+        }
+    },
+
+    /**
+     * Write data to file.
+     *
+     * @tparam  String  data  Data to write.
+     *
+     * @throws  IOError  Thrown on any IO error.
+     * @throws  CharsetConvertError Thrown when charset conversion fails.
+     *
+     * @public
+     */
+    write: function(data)
+    {
+        if (this.__coder)
+            data = this.__coder.encode(data);
+        try {
+            return this.__outputStream.write(data, data.length);
+        } catch (ex) {
+            throw new IOError("File.write: unable to write date", ex);
+        }
+    },
+
+    /**
+     * Close file.
+     *
+     * @public
+     */
+    close: function()
+    {
+        if ("__outputStream" in this && this.__outputStream)
+            this.__outputStream.close();
+        if ("__inputStream" in this && this.__inputStream)
+            this.__inputStream.close();
+        this.__inputStream = this.__outputStream = null;
+    },
+
+    /**
+     * Remove file or directory.
+     *
+     * @tparam  bool  recursive  If set to \c true this operation will
+     *   delete not empty directories with all it contents.
+     *
+     * @throws  IOError  Thrown on any IO error.
+     *
+     * @public
+     */
+    remove: function(recursive)
+    {
+        if (!this.exists)
+            return;
+
+        try {
+            this.close();
+            this.file.remove(recursive);
+        } catch (ex) {
+            throw new IOError("File.remove: IO error", ex);
+        }
+    },
+
+    /**
+     * Flushes output buffer.
+     *
+     * @throws  IOError  Thrown on any IO error.
+     *
+     * @public
+     */
+    flush: function()
+    {
+        try {
+            if (this.__outputStream)
+                this.__outputStream.flush();
+        } catch (ex) {
+            throw new IOError("File.remove: IO error", ex);
+        }
+    },
+
+    /**
+     * Creates new directory.
+     *
+     * @tparam  int  perms  Unix permisions bits for new directory. By
+     *   defaulr value of 0755 will be used. <em>(optional)</em>
+     *
+     * @throws  IOError  Thrown on any IO error.
+     *
+     * @public
+     */
+    createDirectory: function(perms)
+    {
+        try {
+            this.file.create(1, perms == null ? 0755 : perms);
+        } catch (ex) {
+            throw new IOError("File.remove: Unable to create directory", ex);
+        }
+    },
+}
+
