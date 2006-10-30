2984b28b8d60ba0022d15dd54f252701be39acb9
Paweł Chmielowski <pchmielowski@proces-one.net>
2006-10-30 12:22:36 +0000

    Add conference invitation function to contact context menu.

--- chrome/messenger/content/JavaScript/model/account.js
+++ chrome/messenger/content/JavaScript/model/account.js
@@ -429,8 +429,6 @@ _DECL_(Account, null, Model, DiscoItem).prototype =
                 else
                     new Contact(items[i]);
             }
-            var c = new Conference("test@muc.prefiks.ath.cx", "kaban");
-            c.onInvite();
             break;
         }
     },
