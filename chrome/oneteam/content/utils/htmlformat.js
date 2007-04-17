9a8e0426f989ce7b23c4fcca662d08ab5a0bb965
Paweł Chmielowski <pchmielowski@proces-one.net>
2007-04-17 19:22:29 +0000

    Unused files.

--- chrome/oneteam/content/utils/htmlformat.js
+++
@@ -1,6 +0,0 @@
-function html_escape(message){
-        message = message.replace(/\&/g, "&amp;").replace(/\</g, "&lt;")
-                                  .replace(/\>/g, "&gt;");
-        return message;
-}
-
