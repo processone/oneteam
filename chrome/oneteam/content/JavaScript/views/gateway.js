fcfb07b4a525aba6f96acc81a79a21b42302581d
Paweł Chmielowski <pchmielowski@proces-one.net>
2007-05-10 14:28:19 +0000

    Add context menu for gateways icons.

--- chrome/oneteam/content/JavaScript/views/gateway.js
+++ chrome/oneteam/content/JavaScript/views/gateway.js
@@ -101,9 +101,12 @@ function GatewayToolbarButton(model, parentView)
     this.node.setAttribute("autoCheck", "false");
     this.node.setAttribute("tooltiptext", model.gatewayName + " ("+model.jid+")");
     this.node.setAttribute("oncommand", "this.checked ? this.model.logout() : this.model.login()")
+    this.node.setAttribute("context", "gateway-contextmenu");
+    this.node.setAttribute("onmousedown", "this._contextMenu.activeItem = this.model");
 
     this.node.model = this.model;
     this.node.view = this;
+    this.node._contextMenu = document.getElementById('gateway-contextmenu');
 
     this._bundle = new RegsBundle(this);
     this._bundle.register(this.model, this.onPresenceUpdated, "presence");
