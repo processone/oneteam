2b41b46c69c894aebfc245fe2d65a35928f7d1b3
Paweł Chmielowski <pchmielowski@proces-one.net>
2007-08-01 07:51:31 +0000

    Publish in disco/caps info about xhtml-im.

--- chrome/oneteam/content/JavaScript/services/manager.js
+++ chrome/oneteam/content/JavaScript/services/manager.js
@@ -342,3 +342,4 @@ servicesManager.addIQService("jabber:iq:version", function (pkt, query) {
 servicesManager.publishDiscoInfo("http://jabber.org/protocol/disco#info");
 servicesManager.publishDiscoInfo("http://jabber.org/protocol/muc");
 servicesManager.publishDiscoInfo("http://jabber.org/protocol/chatstates", "cs");
+servicesManager.publishDiscoInfo("http://jabber.org/protocol/xhtml-im", "xhtml");
