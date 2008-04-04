9b25d3f474ff220cd2e50c6a884ece1c2b561121
Paweł Chmielowski <pchmielowski@proces-one.net>
2008-04-04 08:45:53 +0000

    "libsub-name-perl in perl" -> "libsub-name-perl in debian"

--- build.pl
+++ build.pl
@@ -8,7 +8,7 @@ use lib qw(tools/perl5lib tools/perl5lib/3rdparty);
 eval {
     require Sub::Name;
 };
-die "Please install perl Sub::Name module (libsub-name-perl in perl)" if $@;
+die "Please install perl Sub::Name module (libsub-name-perl in debian)" if $@;
 
 use File::Find;
 use File::Spec;
