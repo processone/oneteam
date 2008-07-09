3ae569221483252018ec667781eeccdef63fe4ac
Paweł Chmielowski <pchmielowski@proces-one.net>
2008-07-09 10:16:27 +0000

    Change and enable again comments striping function in js files.

--- tools/perl5lib/OneTeam/Builder/Filter/CommentsStripper.pm
+++ tools/perl5lib/OneTeam/Builder/Filter/CommentsStripper.pm
@@ -6,8 +6,31 @@ sub process {
     my ($self, $content, $file) = @_;
 
     if ($file =~ /\.js$/) {
-#        $content =~ s!([^\n\r]*/\*.*?\*[^\n\r]*)/!my $t = $1; $t =~ s{[^\n]}{}g; $t!ges;
-#        $content =~ s![\s\t]*//[^\n\r]*!!g;
+        my $out;
+        my $end = 0;
+        while ($content =~
+                m! \G (?> [^"'/]* )
+                    (?:
+                         (?:" (?:[^\\"]|\\.)* ") |
+                         (?:' (?:[^\\']|\\.)* ') |
+                         (?: / (?:
+                            (/ [^\n]*) |
+                            (\* [\s\S]*? \*\/)
+                         ))
+                    )!xg)
+        {
+            if ($1) {
+                $out .= substr($content, $end, $-[1] - 1 - $end);
+            } elsif ($2) {
+                $out .= substr($content, $end, $-[2] - 1 - $end);
+                (my $nls = $2) =~ s/[^\n]//g;
+                $out .= $nls;
+            } else {
+                $out .= substr($content, $end, $+[0] - $end);
+            }
+            $end = $+[0];
+        }
+        $content = $out . substr($content, $end);
     } elsif ($file =~ /\.(xml|xul)$/) {
         $content =~ s/([^\n\r]*<!--.*?-->[^\n\r]*)/my $t = $1; $t =~ s{[^\n]}{}g; $t/ges;
     }
