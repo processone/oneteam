46b55f373f20c07ccb20bd21f8aafdbf74879322
Paweł Chmielowski <pchmielowski@proces-one.net>
2008-07-14 08:02:12 +0000

    Wrap screensaver headers in correct visibility pragma on linux.

--- componentsSrc/src/idle/otIdleUnix.h
+++ componentsSrc/src/idle/otIdleUnix.h
@@ -1,8 +1,11 @@
 #ifndef _otIDLEXLIB_H_
 #define _otIDLEXLIB_H_
 
+#pragma GCC system_header
+#pragma GCC visibility push(default)
 #include <X11/Xlib.h>
 #include <X11/extensions/scrnsaver.h>
+#pragma GCC visibility pop
 
 #include "nsCOMPtr.h"
 #include "otIdleBase.h"
