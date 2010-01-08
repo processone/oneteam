0cfb7b2a8476c1f770107814f58ddbe643d20ad7
Paweł Chmielowski <pchmielowski@process-one.net>
2010-01-08 16:50:44 +0100

    Build master and flash-jingle branches

--- tools/gen.sh
+++ tools/gen.sh
@@ -7,25 +7,25 @@ if lockfile -r 0 -! update.lock 2>/dev/null; then
     exit
 fi
 
-HEAD=`git rev-list -1 HEAD`
-git pull -q >/dev/null 2>&1
-if [ -n "$1" -o $HEAD != `git rev-list -1 HEAD` ]; then
+function build () {
     rm *.xpi *.xulapp *.mar *.xml 2>/dev/null
 
+    git checkout origin/$1
+
     perl build.pl XULAPP 1 DEBUG 1 \
-	MAR_BASE_URL 'http://dev3.process-one.net/~prefiks/oneteam' \
-	MAR_FILE 'oneteam-@BUILDID@@MAC_SUFFIX@.mar' \
-	MAR_UPDATE_URL 'http://dev3.process-one.net/~prefiks/cgi-bin/update.cgi?q=%PRODUCT%/%VERSION%/%BUILD_ID%/%BUILD_TARGET%/%OS_VERSION%/%DISTRIBUTION%/%DISTRIBUTION_VERSION%/update.xml' \
-	|| { rm -f update.lock; exit; }
+      MAR_BASE_URL "http://dev3.process-one.net/~prefiks/$2" \
+      MAR_FILE 'oneteam-@BUILDID@@MAC_SUFFIX@.mar' \
+      MAR_UPDATE_URL 'http://dev3.process-one.net/~prefiks/cgi-bin/update.cgi?q=%PRODUCT%/%VERSION%/%BUILD_ID%/%BUILD_TARGET%/%OS_VERSION%/%DISTRIBUTION%/%DISTRIBUTION_VERSION%/update.xml' \
+    || { rm -f update.lock; exit; }
     perl build.pl XPI 1 DEBUG 1 UPDATE_URL 'https://dev3.process-one.net/~prefiks/oneteam/update.rdf'
 
-    rm  ../../public_html/oneteam/*.xulapp ../../public_html/oneteam/*-partial.mar ../../public_html/oneteam/*.xpi 2>/dev/null
-    cp *.xulapp *.mar *.xpi ../../public_html/oneteam/
+    rm  ../../public_html/$2/*.xulapp ../../public_html/$2/*-partial.mar ../../public_html/$2/*.xpi 2>/dev/null
+    cp *.xulapp *.mar *.xpi ../../public_html/$2/
     perl -e '
         @files = map { $_->[1] }
           sort { $a->[0] cmp $b->[0] }
           map { my $x = $_; $x =~ s/(\d+)/sprintf "%010d", $1/ge; [$x, $_] }
-          glob("../../public_html/oneteam/*.mar");
+          glob("../../public_html/$ARGV[0]/*.mar");
 
         $files[-1] =~ /.*\.(\d+)/;
         $max = $1;
@@ -48,8 +48,15 @@ if [ -n "$1" -o $HEAD != `git rev-list -1 HEAD` ]; then
             system("tools/make-partial-mar.pl", $_, $new, $partial, "mars-info.txt",
                    "$num", "http://dev3.process-one.net/~prefiks/oneteam/$partialfile", /-mac/ ? 1 : 0);
         }
-    '
+    ' $2
     cp mars-info.txt ..
+}
+
+HEAD=`git rev-list -1 HEAD`
+git pull -q >/dev/null 2>&1
+if [ -n "$1" -o $HEAD != `git rev-list -1 HEAD` ]; then
+  build flash-jingle flash-jingle
+  build master oneteam
 fi
 
 rm -f update.lock
