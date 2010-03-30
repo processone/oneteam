584d4bf9e294bb0e5ae2b11feb0fedc1e2a800e7
Sonny <sonny.piers@gmail.com>
2010-03-30 17:28:32 +0200

    PrefManager now handle unicode string.

--- chrome/oneteam/content/JavaScript/prefs.js
+++ chrome/oneteam/content/JavaScript/prefs.js
@@ -1,10 +1,7 @@
 var EXPORTED_SYMBOLS = ["prefManager"];
 
-//#ifdef XULAPP
-function PrefManager()
-{
-    this.srv = Components.classes["@mozilla.org/preferences;1"].
-        getService(Components.interfaces.nsIPrefBranch2);
+function PrefManager() {
+    this.srv = Components.classes["@mozilla.org/preferences;1"].getService(Components.interfaces.nsIPrefBranch2);
     this.callbacks = {};
 }
 
@@ -48,28 +45,30 @@ _DECL_(PrefManager).prototype =
         }
     },
 
-    getPref: function(name)
-    {
-        try {
-            var type = this.srv.getPrefType(name);
-            return type == this.srv.PREF_BOOL ? this.srv.getBoolPref(name) :
-                type == this.srv.PREF_INT ? this.srv.getIntPref(name) :
-                type == this.srv.PREF_STRING ? this.srv.getCharPref(name) :
-                null;
-        } catch(ex) {
-            return null;
-        }
+    getPref: function(name) {
+      try {
+        var type = this.srv.getPrefType(name);
+        var toto = type == this.srv.PREF_BOOL ? this.srv.getBoolPref(name) :
+          type == this.srv.PREF_INT ? this.srv.getIntPref(name) :
+          type == this.srv.PREF_STRING ? this.srv.getComplexValue(name, Components.interfaces.nsISupportsString).data :
+          null;
+        return toto;
+      } catch(ex) {
+          return null;
+      }
     },
 
-    setPref: function(name, value)
-    {
-        const map = {
-            "number": "setIntPref",
-            "boolean": "setBoolPref"
-        };
-        try {
-            this.srv[ map[typeof(value)] || "setCharPref" ](name, value);
-        } catch(ex) { }
+    setPref: function(name, value) {
+      var valueType = typeof(value);
+      if(valueType == "boolean")
+        this.srv.setBoolPref(name, value);
+      if(valueType == "number")
+        this.srv.setIntPref(name, value);
+      if(valueType == "string"){
+        var str = Components.classes["@mozilla.org/supports-string;1"].createInstance(Components.interfaces.nsISupportsString);
+        str.data = value;
+        this.srv.setComplexValue(name, Components.interfaces.nsISupportsString, str);
+      }
     },
 
     builtinPref: function(name)
@@ -99,124 +98,6 @@ _DECL_(PrefManager).prototype =
         }
     }
 }
-/* #else
-function PrefManager()
-{
-    this.callbacks = {};
-    this.prefs = {
-        @@PREFS@@
-    };
-
-    this.builtinPrefs = {}
-
-    this.storage = new StorageWrapper("prefs");
-
-    for ([key, value] in this.storage) {
-        if ((keyName = key.replace(/^str:/, "")) != key)
-            this.prefs[keyName] = this.storage.get(key);
-        else if ((keyName = key.replace(/^bool:/, "")) != key)
-            this.prefs[keyName] = this.storage.get(key) == "true";
-        else if ((keyName = key.replace(/^int:/, "")) != key)
-            this.prefs[keyName] = +this.storage.get(key);
-    }
-
-    var serverPrefsURL = document.location.href.
-// #ifndef NOJAR
-        replace(/jar:/, "").replace(/\/[^\/]*!.*$/, "/serverPrefs.dat");
-// #else
-        replace(/\/content\/.*$/, "/serverPrefs.dat");
-// #endif
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", serverPrefsURL, false)
-    xhr.send("")
-
-    var lineMatch, valueMatch;
-    while (lineMatch = /(\S+)[^\S\n]*=[^\S\n]*([^\n]+?)[^\S\n]*(?:\n|$)/g.exec(xhr.responseText)) {
-        this.builtinPrefs[lineMatch[1]] = 1;
-        if (valueMatch = lineMatch[2].match(/'((?:[^\\']|\\.)*)'|"((?:[^\\"]|\\.)*)"/))
-            this.prefs[lineMatch[1]] = valueMatch[1]||valueMatch[2];
-        else if (valueMatch = lineMatch[2].match(/^true|(false)$/))
-            this.prefs[lineMatch[1]] = !valueMatch[1];
-        else
-            this.prefs[lineMatch[1]] = +lineMatch[2];
-    }
-}
-
-_DECL_(PrefManager).prototype =
-{
-    registerChangeCallback: function(callback, branch, notifyNow)
-    {
-        if (!this.callbacks[branch]) {
-            this.callbacks[branch] = [callback];
-        } else if (this.callbacks[branch].indexOf(callback) < 0)
-            this.callbacks[branch].push(callback);
-
-        if (!notifyNow)
-            return;
-
-        branch += ".";
-        for (var name in this.prefs)
-            if (name.indexOf(branch) == 0)
-                callback(name, this.prefs[name]);
-    },
-
-    unregisterChangeCallback: function(callback, branch)
-    {
-        if (branch != null) {
-            var r = {};
-            r[branch] = 1;
-            branch = r;
-        } else
-            branch = Iterator(this.callbacks, true);
-
-        for (i in branch) {
-            var idx = this.callbacks[i] && this.callbacks[i].indexOf(callback);
-            if (idx != null && idx >= 0)
-                this.callbacks[i].splice(idx, 1);
-        }
-    },
-
-    getPref: function(name)
-    {
-        var res;
-        if (res = name.match(/chat.connection.(host|port|domain)$/))
-            return account.connectionInfo[res[1]];
-        return this.prefs[name];
-    },
-
-    setPref: function(name, value)
-    {
-        if (this.prefs[name] === value)
-            return;
-
-        const map = {
-            "number": "int:",
-            "boolean": "bool:"
-        };
-
-        this.storage.set((map[typeof(value)] || "str:") + name, value);
-        this.prefs[name] = value;
-
-        for (var i in this.callbacks)
-            if (name == i || name.indexOf(i+".") == 0)
-                for (var j = 0; j < this.callbacks[i].length; j++)
-                    this.callbacks[i][j].call(null, name, value);
-    },
-
-    builtinPref: function(name)
-    {
-        if (name.search(/chat.connection.(host|port|domain)$/) == 0)
-            return true;
-        return name in this.builtinPrefs;
-    },
-
-    deletePref: function(name)
-    {
-        delete this.prefs[name];
-    },
-}
-//#endif */
 
 var prefManager = new PrefManager();
 if (!prefManager.getPref("chat.connection.type")) {
