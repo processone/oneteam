445b370307217128fd97851af436419a85854cd9
Paweł Chmielowski <pchmielowski@process-one.net>
2010-07-12 17:13:11 +0200

    Remove from libspeex autoconf build system and unused files

---
+++ src/components/libs/libspeex/include/speex/speex_config_types.h.cmake
@@ -0,0 +1,11 @@
+#ifndef __SPEEX_TYPES_H__
+#define __SPEEX_TYPES_H__
+
+/* these are filled in by configure */
+typedef @16BIT_TYPE@ spx_int16_t;
+typedef unsigned @16BIT_TYPE@ spx_uint16_t;
+typedef @32BIT_TYPE@ spx_int32_t;
+typedef unsigned @32BIT_TYPE@ spx_uint32_t;
+
+#endif
+
