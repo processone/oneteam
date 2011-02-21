c6427969c73c7edc45471fd45d0c994ff82115a9
Paweł Chmielowski <pchmielowski@process-one.net>
2011-02-21 16:47:38 +0100

    Always use html5 sounds player loaded in hidden window
    This allows us to have looping sounds and cancel playback at any moment

--- chrome/oneteam/content/JavaScript/services/xpcom/utils.js
+++ chrome/oneteam/content/JavaScript/services/xpcom/utils.js
@@ -1,4 +1,6 @@
-var EXPORTED_SYMBOLS = ["findCallerWindow", "soundsPlayer", "DEBUG"];
+var EXPORTED_SYMBOLS = ["findCallerWindow", "soundsPlayer", "DEBUG",
+                        "getHiddenWindowHandle", "execInHiddenWindow",
+                        "getHiddenWindowCommands"];
 
 function findCallerWindow()
 {
@@ -18,80 +20,55 @@ function findCallerWindow()
     return null;
 }
 
+function getHiddenWindowHandle() {
+    return openDialogUniq("ot:hiddenWindow", "chrome://oneteam/content/hiddenWindow.xul",
+                          "chrome,dialog=no,popup")
+}
+
+var commands = []
+function getHiddenWindowCommands() {
+    var cmds = commands;
+
+    commands = [];
+
+    return cmds;
+}
+
+function execInHiddenWindow(arg) {
+    commands.push(arg);
+
+    var w = getHiddenWindowHandle();
+
+    if (w.newCommands)
+        w.newCommands();
+}
+
 var soundsPlayer = {
     playSound: function(type, loops) {
-        try {
-            if (!prefManager.getPref("chat.general.sounds"))
-                return;
+        if (!prefManager.getPref("chat.general.sounds"))
+            return {cancel: function() {}};
 
-            if (this._html5Player == null) {
-                var win = findCallerWindow();
+        var canceler = {
+            cancel: function() {
+                if (this._canceler)
+                    this._canceler.cancel();
+                else
+                    this._cancel = true;
+            },
 
-                if (!win) {
-                    var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].
-                        getService(Components.interfaces.nsIWindowMediator);
-                    var br = wm.getEnumerator("");
-                    win = br.getNext();
-                }
-
-                if (win)
-                    this._html5Player = win.document.createElementNS(HTMLNS, "audio");
-                if (!this._html5Player || !("src" in this._html5Player))
-                    this._html5Player = false;
+            result: function(canceler) {
+                if (this._cancel)
+                    canceler.cancel();
+                else
+                    this._canceler = canceler;
             }
+        }
 
-            if (this._html5Player) {
-                this._html5Player.src = "chrome://oneteam/content/data/sounds/"+
-                    type+".wav";
-                this._html5Player.load();
-                this._html5Player.play();
-                return;
-            }
+        execInHiddenWindow(["playSound", canceler,
+                            "chrome://oneteam/content/data/sounds/"+type+".wav",
+                            loops]);
 
-            if (this._player == null) {
-                try {
-                    this._player = Components.classes["@mozilla.org/sound;1"].
-                        createInstance(Components.interfaces.nsISound);
-                    this._ios = Components.classes["@mozilla.org/network/io-service;1"].
-                        getService(Components.interfaces.nsIIOService);
-                } catch (ex) {
-                    this._player = false;
-                }
-            }
-
-            if (this._player) {
-                if (!this._threadCreated && !this._thread) {
-                    this._threadCreated = true;
-                    try {
-                        if (navigator.platform.search(/linux/i) >= 0)
-                        this._thread = Components.classes["@mozilla.org/thread-manager;1"].
-                            getService(Components.interfaces.nsIThreadManager).newThread(0);
-                    } catch (ex) {}
-                }
-                var url = this._ios.newURI("chrome://oneteam/content/data/sounds/"+
-                                           type+".wav", null, null);
-                if (this._thread) {
-                    if (!this._playing || !this._playing.value) {
-                        if (!this._playing)
-                            this._playing = {};
-
-                        this._playing.value = true;
-
-                        this._thread.dispatch({
-                            run: function() {
-                                this.player.play(this.url);
-                                this.playing.value = false;
-                            },
-
-                            player: this._player,
-                            playing: this._playing,
-                            url: url
-                        }, this._thread.DISPATCH_NORMAL);
-                    }
-                } else
-                    this._player.play(url);
-            }
-        } catch(ex){ dump(ex+"\n")}
+        return canceler;
     }
 };
 
