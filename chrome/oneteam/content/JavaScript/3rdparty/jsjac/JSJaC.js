ea5a27d73dd104b6b3ec947baa259ded1fa487f4
Paweł Chmielowski <pchmielowski@process-one.net>
2011-06-23 01:39:26 +0200

    Drop web scripts loader from

--- chrome/oneteam/content/JavaScript/3rdparty/jsjac/JSJaC.js
+++ chrome/oneteam/content/JavaScript/3rdparty/jsjac/JSJaC.js
@@ -9,40 +9,7 @@ var EXPORTED_SYMBOLS = ["JSJaC"];
 
 var JSJaC = {
   Version: '$Rev$',
-  require: function(libraryName) {
-    // inserting via DOM fails in Safari 2.0, so brute force approach
-    document.write('<script type="text/javascript" src="'+libraryName+'"></script>');
-  },
-  load: function() {
-    var includes =
-    ['xmlextras',
-     'jsextras',
-     'crypt',
-     'JSJaCConfig',
-     'JSJaCConstants',
-     'JSJaCCookie',
-     'JSJaCJSON',
-     'JSJaCJID',
-     'JSJaCBuilder',
-     'JSJaCPacket',
-     'JSJaCError',
-     'JSJaCKeys',
-     'JSJaCConnection',
-     'JSJaCHttpPollingConnection',
-     'JSJaCHttpBindingConnection',
-     'JSJaCConsoleLogger'
-     ];
-    var scripts = document.getElementsByTagName("script");
-    var path = './';
-    for (var i=0; i<scripts.length; i++) {
-      if (scripts.item(i).src && scripts.item(i).src.match(/JSJaC\.js$/)) {
-        path = scripts.item(i).src.replace(/JSJaC.js$/,'');
-        break;
-      }
-    }
-    for (var i=0; i<includes.length; i++)
-      this.require(path+includes[i]+'.js');
-  },
+
   bind: function(fn, obj, arg) {
     return function() {
       if (arg)
